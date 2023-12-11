a160b4a73eb453d3488105471ed37e93aa076302
Tom Williams <104518122+aurongard@users.noreply.github.com>
2023-12-11 17:44:39 -0500

    Add files via upload

---
+++ Scooter.cs
@@ -0,0 +1,313 @@
+﻿//Filname Scooter.cs
+//Written by Tom Williams
+//Written on 11/03/23
+
+namespace Scooter
+{
+    using System;
+    using System.Collections;
+    using static System.Console;
+
+    public class Scooter
+    {
+        private String app; //name of the app used
+        private String model; //model of the scooter
+        private int year; //year of the scooter
+        private double hours; //hours the scooter has been ridden
+        private double hoursNextSoftwareUpdate; //hours until the next software update
+        private double charge; // charge level of the battery
+        private double wph; //watts per hour of the scooter
+        private const double BatteryCapacity = 250.0; //max battery capacity
+        private const int HoursBetweenUpdates = 48; //how many hours before next update
+        private bool scooterLock;
+
+        public Scooter() //default constructor
+        {
+            app = "Spin";
+            model = "SegwayES4";
+            year = 2021;
+            hours = 0.0;
+            wph = 20.0;
+            charge = 250.0;
+            hoursNextSoftwareUpdate = 48;
+            scooterLock = true;
+        }
+        public Scooter(String App, String Model, int y, double Wph) //constructor with additional input
+        {
+            app = App;
+            model = Model;
+            year = y;
+            wph = Wph;
+            hours = 0.0;
+            charge = 250.0;
+            hoursNextSoftwareUpdate = 48;
+            scooterLock = true;
+        }
+
+        //Getters and Setters
+        public String App
+        {
+            get { return app; }
+            set { app = value; }
+        }
+        public String Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        public double Hours
+        {
+            get { return hours; }
+            set { hours = value; }
+        }
+        public double HoursNextSoftwareUpdate
+        {
+            get { return hoursNextSoftwareUpdate; }
+            set { hoursNextSoftwareUpdate = value; }
+        }
+        public double Charge
+        {
+            get { return charge; }
+            set { charge = value; }
+        }
+        public double Wph
+        {
+            get { return wph; }
+            set { wph = value; }
+        }
+
+        //unlocks scooter
+        public void UnlockScooter()
+        {
+            scooterLock = true;
+            WriteLine("{0} {1} {2} unlocked", App, Model, Year);
+        }
+
+        //locks scooter
+        public void LockScooter()
+        {
+            scooterLock = false;
+            WriteLine("{0} {1} {2} locked", App, Model, Year);
+        }
+
+        //checks status of scooter's lock
+        public bool IsUnlocked ()
+        {
+            return scooterLock;
+        }
+
+        //method to charge the battery
+        public void ChargeBattery(double chargeAmt)
+        {
+            if (scooterLock == false) //checks if scooter is locked
+            {
+                if (chargeAmt < 0.0) // if charge amount is negative it gives a warning
+                {
+                    WriteLine("{0} {1} {2} watts cannot be a negative number - Charge in Battery after being plugged in: {3}", App, Model, Year, Charge);
+                }
+                else
+                {
+                    Charge += chargeAmt; //applies the charge amount to the total battery charge.
+
+                    if (Charge > 250.0) //gives warning if battery is overcharged after charging.
+                    {
+                        Charge = 250.0; //sets charge to max without being over.
+                        WriteLine("{0} {1} {2} battery overcharged - Charge in Batter after being plugged in: {3}", App, Model, Year, Charge);
+                    }
+                    else
+                    {
+                        WriteLine("{0} {1} {2} added charge {3} - Charge in Batter after being plugged in: {4}", App, Model, Year, chargeAmt, Charge); //if everything is nominal.
+                    }
+                }
+            }
+            else
+            {
+                WriteLine("{0} {1} {2} must be locked to charge battery", App, Model, Year);
+            }
+        }
+
+        //method to add ridden hours to the scooter and update the battery and ride hours as necessary
+        public void Ride(double rideHours)
+        {
+            double wattsUsed;
+            double tempBatt;
+            double cutRide;
+
+            if (scooterLock == true) //checks to make sure that scooter is unlocked
+            {
+                if (rideHours < 0.0) //if ride hours is negative, gives this error and does not update battery charge.
+                {
+                    WriteLine("{0} {1} {2} watts cannot ride negative hours.", App, Model, Year);
+                }
+                else
+                {
+                    wattsUsed = rideHours * Wph; //gets the number of battery power needed for total ride.
+                    tempBatt = Charge - wattsUsed; //creates temp variable for how much charge was used
+
+                    if (tempBatt < 0.0) //checks if charge goes into the negatives
+                    {
+                        scooterLock = false;
+                        cutRide = (wattsUsed + tempBatt) / Wph; //calculates the number of hours were ridden before the battery ran out.
+                        Charge = 0.0; //sets battery to empty.
+                        Hours += cutRide; //adds to hours ridden.
+                        WriteLine("{0} {1} {2} ran out of battery after riding {3} hours.", App, Model, Year, cutRide);
+                    }
+                    else //if scooter is ridden and doesn't run out of battery.
+                    {
+                        Hours += rideHours; //adds to hours ridden
+                        Charge = tempBatt; //updates the battery charge
+                        WriteLine("{0} {1} {2} rode {3} hours.", App, Model, Year, rideHours);
+                    }
+                }
+            }
+            else
+            {
+                WriteLine("{0} {1} {2} must be UNLOCKED to ride.", App, Model, Year);
+            }
+        }
+
+        //method to mark next inspection.
+        public void Inspect()
+        {
+            if (scooterLock == false) 
+            {
+                double inspection = hours + 48;
+                hoursNextSoftwareUpdate = inspection;
+                WriteLine("{0} {1} {2} has been updated, next update is: {3}", App, Model, Year, hoursNextSoftwareUpdate);
+            }
+            else
+            {
+                WriteLine("{0} {1} {2} must be locked to inspect software.", App, Model, Year);
+            }
+            
+        }
+
+        //method to check for update
+        public void CheckforUpdate()
+        {
+            if (scooterLock == false)
+            {
+                if (hours >= hoursNextSoftwareUpdate) //if hours ridden are equal to or greater than update marker - need to update
+                {
+                    WriteLine("{0} {1} {2} - It is time to update software.", App, Model, Year);
+                }
+                else if (hours < hoursNextSoftwareUpdate) //if hours ridden are less than the update marker - no need to update
+                {
+                    WriteLine("{0} {1} {2} - Scooter is OK, no need to update software.", App, Model, Year);
+                }
+            }
+            else
+            {
+                WriteLine("{0} {1} {2} must be locked to check for updates.", App, Model, Year);
+            }
+        }
+
+        //method to calculate remaining time scooter can be ridden
+        private double CalcRange()
+        {
+            double range = charge / wph;
+            return range;
+        }
+
+        //method to calculate how much the battery needs to charge
+        private double ChargeNeededToFullyChargeBattery()
+        {
+            double chargeNeeded = BatteryCapacity - charge;
+            return chargeNeeded;
+        }
+
+        //method string override to return, but not print scooter details
+        public override String ToString ()
+        {
+            String ScooterDetail = App + " " + Model + " " + Year + " Hours: " + hours.ToString("N") + " Battery charge: " + charge.ToString("N");
+            return ScooterDetail;
+        }
+
+        public void SimulateMultiUserRides(int numberOfUsers)
+        {
+            double maxTime = CalcRange() * 60; //calls the maximum time the scooter can be ridden and converts it to minutes
+            double userTime; //used to convert the random number of minutes back to hours.
+            double tempBattUsage; //calculates battery used on the trip
+            double wattsUsed; //calculates the watts used
+            double cutRide = 0.0; //variable for time ridden when battery dies
+            int userRide; //variable for the number of minutes a user rides the scooter for.
+            int userTotRide = 0; //holds total user time ridden.
+            int maxRide = Convert.ToInt32 (maxTime); //converts maxTime into an int for the random number generator
+            int userRiders = 0; //tracks how many riders actually rode the scooter before battery died or no more users
+            int i; //index for "for loops" to print user times
+
+            ArrayList riderNum = new ArrayList(); //holds the rider number
+            ArrayList riderTimes = new ArrayList(); //holds the rider times
+
+            Random random = new Random();
+            if (charge > 0.0)
+            {
+                while ((maxTime > 0.0) || (numberOfUsers > 0))
+                {
+                    ++userRiders; //increments the number of riders
+                    riderNum.Add(userRiders); //adds rider number to list of riders
+
+                    userRide = random.Next(1, maxRide); //generates number of minutes the ride was for current rider
+                    userTime = userRide / 60.0; //converts the minutes back into hours.
+
+                    //riderTimes.Add(userRide); //adds the time ridden for individual rider
+
+                    tempBattUsage = charge - (wph * userTime);
+
+                    if (tempBattUsage < 0.0)
+                    {
+                        wattsUsed = userTime * wph;
+                        cutRide = ((wattsUsed + tempBattUsage) / wph) * 60; //calculates in minutes how long the scooter was ridden before batter dies
+                        userTotRide += Convert.ToInt32(cutRide);
+                        maxTime -= cutRide; //sets time to 0.0
+                    }
+                    else
+                    {
+                        maxTime -= userRide; //reduces time before battery dies
+                        userTotRide += userRide;
+                    }
+
+                    UnlockScooter(); //unlocks scooter to be ridden.
+
+                    Ride(userTime); //enters in the ride time the user rides the scooter.
+
+                    if (charge == 0.0)
+                    {
+                        userRide = Convert.ToInt32(cutRide);
+                        riderTimes.Add(userRide); //adds the time ridden for individual rider
+                        LockScooter(); //locks the scooter.
+
+                        WriteLine();
+                        WriteLine("Riders before battery reached 0.0:");
+                        WriteLine("--------------------------------------------");
+
+                        for (i = 0; i < riderNum.Count; i++)
+                        {
+                            WriteLine("Rider #" + riderNum[i] + " rode for " + riderTimes[i] + " minutes.");
+                        }
+                        WriteLine("--------------------------------------------");
+                        WriteLine("Total Riders of " + userRiders + " rode for " + userTotRide + " minutes.");
+                        break;
+                    }
+                    else
+                    {
+                        riderTimes.Add(userRide); //adds the time ridden for individual rider
+                    }
+
+                    LockScooter(); //locks the scooter.
+                    --numberOfUsers; //reduces the number of people in que to ride.
+
+                }
+            }
+            else
+            {
+                WriteLine("Scooter battery needs to be charged.");
+            }
+        }
+    }
+}
---
+++ ScooterManager.cs
@@ -0,0 +1,362 @@
+﻿//Filname ScooterManager.cs
+//Written by Tom Williams
+//Written on 11/03/23
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scooter
+{
+    using static System.Console;
+    internal class ScooterManager
+    {
+        static void Main(String[] args)
+        {
+
+            WriteLine("**Testing starts");
+            WriteLine("----------------");
+
+            //testing default constructor
+            Scooter scoot = new Scooter();
+            int errors = 0;
+
+            Console.WriteLine("**Testing first constructor");
+
+            //checks app name
+            if (scoot.App.Equals("Spin"))
+            {
+                WriteLine(" + Default App property is working");
+            }
+            else
+            {
+                WriteLine(" - Default App property failed");
+                ++errors;
+            }
+
+            //checks scooter model name
+            if (scoot.Model.Equals("SegwayES4"))
+            {
+                WriteLine(" + Default Model property is working");
+            }
+            else
+            {
+                WriteLine(" - Default Model property failed");
+                ++errors;
+            }
+
+            //checks year
+            if (scoot.Year.Equals(2021))
+            {
+                WriteLine(" + Default Year property is working");
+            }
+            else
+            {
+                WriteLine(" - Default Year property failed");
+                ++errors;
+            }
+
+            //checks hours
+            if (scoot.Hours.Equals(0.0))
+            {
+                WriteLine(" + Default Hours property is working");
+            }
+            else
+            {
+                WriteLine(" - Default Hours property failed");
+                ++errors;
+            }
+
+            //checks wph
+            if (scoot.Wph.Equals(20.0))
+            {
+                WriteLine(" + Default WPH property is working");
+            }
+            else
+            {
+                WriteLine(" - Default WPH property failed");
+                ++errors;
+            }
+
+            //checks charge
+            if (scoot.Charge.Equals(250.0))
+            {
+                WriteLine(" + Default Charge property is working");
+            }
+            else
+            {
+                WriteLine(" - Default Charge property failed");
+                ++errors;
+            }
+
+            //checks hoursNextSoftwareUpdate
+            if (scoot.HoursNextSoftwareUpdate.Equals(48))
+            {
+                WriteLine(" + Default hoursNextSoftwareUpdate property is working");
+            }
+            else
+            {
+                WriteLine(" - Default hoursNextSoftwareUpdate property failed");
+                ++errors;
+            }
+
+            WriteLine();
+
+            //Test number 2 based off of baseline given by Professor to test the parameter input for the second constructor
+            Scooter s = new Scooter("Bird", "Razor", 2019, 30.0);
+
+            Console.WriteLine("**Testing second constructor");
+
+            //Checks app name
+            if (s.App.Equals("Bird"))
+            {
+                WriteLine(" + App property is working");
+            }
+            else
+            {
+                errors++;
+            }
+
+            //checks scooter model name
+            if (s.Model.Equals("Razor"))
+            {
+                WriteLine(" + Model property is working");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            //checks year
+            if (s.Year.Equals(2019))
+            {
+                WriteLine(" + Year property is working");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            //checks wph
+            if (s.Wph.Equals(30.0))
+            {
+                WriteLine(" + WPH property is working");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            WriteLine();
+
+            Console.WriteLine("**Testing setters");
+            //test setter for app
+
+            scoot.App = "Wheelz";
+
+            if (scoot.App.Equals("Wheelz"))
+            {
+                WriteLine(" + App setter property is working");
+            }
+            else
+            {
+                WriteLine(" - App setter property failed");
+                ++errors;
+            }
+
+            s.Model = "Griffyn";
+
+            //checks scooter model name
+            if (s.Model.Equals("Griffyn"))
+            {
+                WriteLine(" + Model setter property is working");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            WriteLine();
+            WriteLine("**Testing Scooter Lock/Unlock methods");
+
+            scoot.Charge = 200.0;
+
+            WriteLine("**Scooter locked for testing.");
+            scoot.LockScooter(); //lock the scooter to allow charging
+
+            //tests if the lock scooter method works
+            if (scoot.IsUnlocked() == false)
+            {
+                WriteLine(" + LockScooter property works");
+                WriteLine(" + IsUnlocked property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            WriteLine("**Scooter unlocked for testing.");
+            scoot.UnlockScooter(); //unlock the scooter to allow charging
+            //tests if the lock scooter method works
+            if (scoot.IsUnlocked() == true)
+            {
+                WriteLine(" + UnlockScooter property works");
+                WriteLine(" + IsUnlocked property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            WriteLine();
+            WriteLine("**Testing charge battery method");
+            scoot.LockScooter();
+
+            //checks batteryCharge method for nominal input
+            scoot.ChargeBattery(10.1);
+            if (scoot.Charge == 210.1)
+            {
+                WriteLine(" + ChargeBattery property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            //checks batteryCharge method for negative input catch
+            scoot.ChargeBattery(-1.0);
+            if (scoot.Charge == 210.1)
+            {
+                WriteLine(" + ChargeBattery negative number catch property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            //checks battryCharge method for over charged catch
+            scoot.ChargeBattery(9000.1);
+            if (scoot.Charge == 250.0)
+            {
+                WriteLine(" + ChargeBattery overcharge property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            WriteLine();
+            WriteLine("**Testing Ride method");
+
+            //tests Ride method
+            scoot.UnlockScooter();
+            scoot.Hours = 0.0;
+            scoot.Charge = 50;
+            scoot.Wph = 25.0;
+
+            scoot.Ride(3);
+
+            //checks Ride methods run out of battery catch
+            if ((scoot.Charge == 0.0) && (scoot.Hours == 2.0))
+            {
+                WriteLine(" + Ride battery empty catch property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            //checks if scooter locks after running out of battery
+            scoot.Ride(1);
+            if (scoot.IsUnlocked() == false)
+            {
+                WriteLine(" + Run out of battery charge scooter lock property works.");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            s.UnlockScooter();
+            s.Hours = 10.0;
+            s.Charge = 250.0;
+            s.Wph = 25.0;
+
+            s.Ride(-0.5);
+
+            //checks Ride methods negative input catch
+            if ((s.Charge == 250.0) && (s.Hours == 10.0))
+            {
+                WriteLine(" + Ride negative catch property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            s.Ride(3.5);
+
+            //checks Ride nominal ride
+            if ((s.Charge == 162.5) && (s.Hours == 13.5))
+            {
+                WriteLine(" + Ride nominal addition property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            WriteLine();
+            WriteLine("**Testing Inspect and Update methods");
+
+            //tests inspect and check for update methods
+            s.LockScooter();
+            s.Hours = 20.0;
+            s.Inspect();
+            s.CheckforUpdate();
+
+            //checks that scooter inspection has updated hours
+            if (s.HoursNextSoftwareUpdate == 68.0)
+            {
+                WriteLine(" + Inspection property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            s.Hours = 69.0;
+            s.CheckforUpdate();
+
+            String details = s.ToString();
+
+            WriteLine(details);
+
+            if (details.Equals("Bird Griffyn 2019 Hours: 69.00 Battery charge: 162.50"))
+            {
+                WriteLine(" + ToString property works");
+            }
+            else
+            {
+                ++errors;
+            }
+
+            //tests simulation
+            WriteLine();
+            WriteLine("**Scooter Simulation");
+            Scooter userScoot = new Scooter("Condor", "Spinster", 2021, 30.0);
+
+            userScoot.SimulateMultiUserRides(5);//runs simulation
+
+            //shows number of errors
+            WriteLine();
+            WriteLine("**Testing ends with " + errors + " errors");
+            WriteLine("---------------------------------------------------------------");
+            WriteLine();
+
+            
+        }
+    }
+}
